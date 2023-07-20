0e5ad3f6addd01ed9215ecf8677f7f74d8be376e
Luka Kažić <kazicluka4@gmail.com>
2023-07-20 02:38:15 +0200

    testing rebase

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
 
   const handleFilters = (filters) => {
     const [anada, nyx] = filters;
+    console.log(anada)
     if (anada || nyx) {
       setData(initialData.filter((item) => {
         if(item.brand === anada || item.brand === nyx) return item;
